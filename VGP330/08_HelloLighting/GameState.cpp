2af50f802a09ed54f0b2adcb8b39d878d645d291
AlexY23333 <tianze.yuan@lcieducation.net>
2025-10-06 19:00:20 -0700

    set up the start up project08

---
+++ VGP330/08_HelloLighting/GameState.cpp
@@ -0,0 +1,84 @@
+#include "GameState.h"
+using namespace std;
+using namespace SumEngine;
+using namespace SumEngine::Math;
+using namespace SumEngine::Graphics;
+using namespace SumEngine::Core;
+using namespace SumEngine::Input;
+
+
+void GameState::Initialize()
+{
+	// Set Cameras
+	mCamera.SetPosition({ 0.0f, 150.0f, -600.0f });
+	mCamera.SetLookAt({ 0.0f, 0.0f, 0.0f });
+}
+
+void GameState::Terminate()
+{
+	
+}
+
+void GameState::UpdateCamera(float deltaTime)
+{
+	auto input = InputSystem::Get();
+	const float moveSpeed = (input->IsKeyDown(KeyCode::LSHIFT) ? 500.0f : 100.0f) * deltaTime;
+	const float turnSpeed = 0.1f * deltaTime;
+	if (input->IsKeyDown(KeyCode::W))
+	{
+		mCamera.Walk(moveSpeed);
+	}
+	else if (input->IsKeyDown(KeyCode::S))
+	{
+		mCamera.Walk(-moveSpeed);
+	}
+	if (input->IsKeyDown(KeyCode::D))
+	{
+		mCamera.Strafe(moveSpeed);
+	}
+	else if (input->IsKeyDown(KeyCode::A))
+	{
+		mCamera.Strafe(-moveSpeed);
+	}
+	if (input->IsKeyDown(KeyCode::E))
+	{
+		mCamera.Rise(moveSpeed);
+	}
+	else if (input->IsKeyDown(KeyCode::Q))
+	{
+		mCamera.Rise(-moveSpeed);
+	}
+	if (input->IsMouseDown(MouseButton::RBUTTON))
+	{
+		mCamera.Yaw(input->GetMouseMoveX() * turnSpeed);
+		mCamera.Pitch(input->GetMouseMoveY() * turnSpeed);
+	}
+}
+
+float totalTime = 0.0f;
+
+void GameState::Update(float deltaTime)
+{
+	totalTime += deltaTime / 10.0f; 
+	UpdateCamera(deltaTime);
+}
+
+int currentRenderTarget = 0;
+float renderTargetDistance = -300.0f;
+bool ringsToggle = true;
+
+void GameState::Render()
+{
+	SimpleDraw::Render(mCamera);
+	
+
+}
+
+bool buttonValue = false;
+
+void GameState::DebugUI()
+{
+	ImGui::Begin("Debug", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+
+	ImGui::End();
+}
---
+++ VGP330/08_HelloLighting/GameState.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SumEngine/Inc/SumEngine.h>
+
+class GameState : public SumEngine::AppState
+{
+public:
+	void Initialize() override;
+	void Terminate() override;
+	void Render() override;
+	void DebugUI() override;
+	void Update(float deltaTime) override;
+
+protected:
+	void UpdateCamera(float deltaTime);
+
+	SumEngine::Graphics::Camera mCamera;
+};
